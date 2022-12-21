6d5b82e0ce626c646e0f7739b2ca9a3210cc5f41
alexbarker <alexandersbarker89@gmail.com>
2022-12-21 22:27:54 +1100

    added question response into database

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = 3000;
 app.get("/api/hello", (req,res) => {
    res.json({message: "hello"}) 
 })
-// get request to trivia API for 10x questions
+// get request to trivia API for 10x questions and adds to database
 app.get("/api/trivia/random10", (req,res) =>{
 axios.get("https://the-trivia-api.com/api/questions?limit=10")
     .then(function(response){
@@ -22,11 +22,15 @@ axios.get("https://the-trivia-api.com/api/questions?limit=10")
             const {category, difficulty, question, correctAnswer, incorrectAnswers} = element
             const [answer1, answer2, answer3] = incorrectAnswers
             console.log(answer1)
-        });
-
+            const sql =`
+            INSERT INTO questions(category, difficulty, question, answer1, answer2, answer3, correct_answer)
+            VALUES($1,$2,$3,$4,$5,$6,$7)`
+            db.query(sql,[category, difficulty, question, answer1, answer2, answer3, correctAnswer]).then(()=>{
+                console.log('questions added to database')
+            })
+        })
     })
     
-
 })
 
 
