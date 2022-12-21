5746ca5e08aa53baad70fc66de15e5c6b92194fe
alexbarker <alexandersbarker89@gmail.com>
2022-12-21 21:35:32 +1100

    created first api call to trivia API

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const db = require("./db/db.js");
+const bodyParser = require('body-parser')
+const axios = require('axios').default;
+
 
 const app = express();
 
@@ -9,6 +13,21 @@ const PORT = 3000;
 app.get("/api/hello", (req,res) => {
    res.json({message: "hello"}) 
 })
+// get request to trivia API for 10x questions
+app.get("/api/trivia/random10", (req,res) =>{
+axios.get("https://the-trivia-api.com/api/questions?limit=10")
+    .then(function(response){
+        const APIResponse = response.data
+        APIResponse.forEach(element => {
+            console.log(element)
+            console.log(`each element here... ${element}`)
+        });
+
+    })
+    
+
+})
+
 
 
 
