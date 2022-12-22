57ca85d186d4193996ce2a460d585c4813e0bb9a
alexbarker <alexandersbarker89@gmail.com>
2022-12-22 21:01:25 +1100

    added create quiz form

---
+++ static/js/components/createQuiz.js
@@ -0,0 +1,30 @@
+export function createQuiz() {
+    const page = document.getElementById('page')
+    page.replaceChildren()
+
+    const quizForm = `
+    <form action="/static/index.html" method="get">
+    <label for="Quiz Name">Quiz Name</label>
+    <input type="text" name="Quiz Name" id="Quiz Name">
+    <label for="category">Pick a category</label>
+    <select name="category" id="category">
+        <option value="General Knowledge">General Knowledge</option>
+        <option value="Society & Culture">Society & Culture</option>
+        <option value="History">History</option>
+        <option value="Sport & Leisure">Sport & Leisure</option>
+        <option value="Geography">Geography</option>
+        <option value="Music">Music</option>
+        <option value="Film & TV">Film & TV</option>
+        <option value="Science">Science</option>
+        <option value="Food & Drink">Food & Drink</option>
+        <option value="Arts & Literature">Arts & Literature </option>
+        <option value="Random">Random</option>
+            <input type="submit" value="submit">
+    </select>
+
+</form>
+    
+    `
+page.innerHTML=quizForm
+
+}
