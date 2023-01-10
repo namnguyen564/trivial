bae599af9e84463816238d4d0cef97ba872d5a4b
Nguyen <@gmail.com>
2023-01-10 13:30:21 +1100

    test

--- static/js/components/renderSignUpPage.js
+++ static/js/components/renderSignUpPage.js
@@ -35,7 +35,7 @@ export function renderSignUpPage(){
         axios
             .post("/users", data)
             .then((response) =>{
-                console.log("congrats")
+                console.log(response)
             })
 
     })
