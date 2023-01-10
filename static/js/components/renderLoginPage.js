cdfe4770c2c68a21df0348cb5b46234283b999ed
Nguyen <@gmail.com>
2023-01-10 20:31:07 +1100

    fixing conflicts

--- static/js/components/renderLoginPage.js
+++ static/js/components/renderLoginPage.js
@@ -1,14 +1,21 @@
+import { renderSignUpPage } from './renderSignUpPage.js'
+import { verifyLogin } from './verifyLogin.js'
+
 export function renderLoginPage() {
     const header = document.getElementById("header-nav");
     header.innerHTML = `
        <h1 id='title'>TRIVIAL</h1>
+       
+       <form id="login-form">
        <input type="text" name="email" id="email-field" class="login-form-field" placeholder="Email">
        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
        <input type="submit" value="Login" id="login-form-submit">
-
-       <input type="submit" value="Sign Up" id="test-form-submit">
-       <input type="submit" value="Guest Login" id="test-form-submit">
+       </form>
+       <input type="submit" value="Sign Up" id="signUpButton">
+       <input type="submit" value="Guest Login" >
       `;
 
       
-  }
+    document.getElementById('signUpButton').addEventListener('click', renderSignUpPage)
+    document.getElementById('login-form-submit').addEventListener('click', verifyLogin)
+}
