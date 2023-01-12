9f91c90183c369f728a2ae7a323e4861c5e7513e
alexbarker <alexandersbarker89@gmail.com>
2023-01-12 19:49:51 +1100

    refactored create quiz now working

--- static/js/components/createQuiz.js
+++ static/js/components/createQuiz.js
@@ -5,9 +5,9 @@ export function renderNewQuizForm() {
     page.replaceChildren()
 
     const quizForm = `
-    <form action="/api/trivia/" method="get">
+    <form id="CreateQuizForm">
     <label for="Quiz Name">Quiz Name</label>
-    <input type="text" name="Quiz Name" id="Quiz Name">
+    <input type="text" name="Quiz Name" id="quizName">
     <label for="category">Pick a category</label>
     <select name="category" id="category">
         <option value="General Knowledge">General Knowledge</option>
@@ -31,5 +31,14 @@ export function renderNewQuizForm() {
 
 page.innerHTML=quizForm
 // addEventListener('submit', renderQuizList)
-
+const form = document.getElementById("CreateQuizForm")
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    const category = document.getElementById("category").value
+    const quizName = document.getElementById("quizName").value
+    const data = {category,quizName}
+    axios.post("/api/trivia", data).then(function(){
+        renderQuizList()
+    })
+})
 }
